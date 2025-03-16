7b95c90e02dd7a21a1c55d1fd35f5a9a230855bb
Jaypaul <jaypaulb@gmail.com>
2025-03-16 14:34:26 +0100

    Update .env to include PORT variable set to 443 for improved network configuration.

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [{
+    name: "canvus-webui",
+    script: "./server.js",
+    interpreter: "authbind",
+    interpreter_args: "--deep node",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+      PORT: 443
+    }
+  }]
+} 
